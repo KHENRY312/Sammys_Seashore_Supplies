c01cbed9d7caffdc528ae37072229c8b5872706a
Kim <kimberly.henry@outlook.com>
2022-12-08 03:55:15 -0500

    SSS-2 CH01 SammysMotto

---
+++ src/ch01/SammysMotto.java
@@ -0,0 +1,17 @@
+package ch01;
+
+/*
+Project:           Sammys_Seashore_Supplies
+Module:         Sammys_Seashore_Supplies
+Package:        ch01
+File:               SammysMotto
+Created By:   Kimberly Henry
+Created On:  Thursday, 08-Dec-2022
+*/
+public class SammysMotto
+	{
+		public static void main(String[] args)
+			{
+				System.out.println("Sammy's makes it fun in the sun.");
+			}
+	}
