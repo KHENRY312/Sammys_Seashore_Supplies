4569d8157cd188e8c988d8a5c38426e6b71a8951
Kim <kimberly.henry@outlook.com>
2022-12-08 05:02:32 -0500

    SSS-1 Case Study Sammy's Seashore Supplies Copyright added to files.

--- src/ch01/SammysMotto2.java
+++ src/ch01/SammysMotto2.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
 package ch01;
 
 /*
